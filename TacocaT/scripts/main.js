a64443344cdd2bb6a3e957da2fb325dcd106b6f2
2ShortGaming <66035017+2ShortGaming@users.noreply.github.com>
2020-07-14 17:45:26 -0400

    updates
    updated prism

---
+++ TacocaT/scripts/main.js
@@ -0,0 +1,16 @@
+﻿$("#btn").on("click", function () {
+    let forward = $("input").val().toLowerCase().replace(/\s/g, '').replace(/\./g, '').replace(/\,/g, '').replace(/\?/g, '');
+
+    let reverse = forward.split("").reverse().join("");
+    if (forward == reverse) {
+        $("#result").text(`The word "${forward}" is a Palindrome!`);
+    }
+    else {
+        $("#result").text(`The word "${forward}" is NOT a Palindrome!`);
+    }
+});
+
+$("#btnClear").on("click", function () {
+    $("#input").val("");
+    $("#result").text("");
+})
